024e65a707bbd44573d7907f900d7d213ec3bedd
Pietro Scurti <pietroscurti9@gmail.com>
2026-01-19 17:21:31 +0100

    sezione card completata

--- dc-comics/src/components/CardSection.jsx
+++ dc-comics/src/components/CardSection.jsx
@@ -10,24 +10,43 @@ export default function CardSection() {
 
     return (
 
-        <section>
+        <section className='card-section'>
             <div className="card-s-container">
 
-                <figure>
-                    <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" alt="" classNames='card-section-img' />
-                    <figcaption>
-                        
+                {comics.map((comic) => {
 
-                    </figcaption>
-                </figure>
+                    return (
+                        <figure>
+                            <img src={comic.thumb} alt="" className='card-section-img'/>
+                            <figcaption className= "c-white">
+                                {comic.title}
+                            </figcaption>
+                        </figure>
+                    )
+                  }
+                 )
+                }
 
 
-
-
-
+                
 
             </div>
         </section>
+
+
+
+
+
+
+
+
+               
+
+
+
+
+
+
 
 
 
